cce1566f99d17db88173cba34cc33a09c4bebb15
Andrew Comminos <andrewcomminos@gmail.com>
2013-06-20 17:17:51 -0700

    Added audio input.

--- scrapstream/ffmpeg_manager.py
+++ scrapstream/ffmpeg_manager.py
@@ -5,8 +5,9 @@ from stream_settings import StreamSettings
 ### Manage the FFMpeg process that is streaming.
 class FFMpegManager:
 
-    command = "ffmpeg -f x11grab -s %(INPUT_WIDTH)dx%(INPUT_HEIGHT)d -r %(FPS)d -i %(DISPLAY)s+%(INPUT_X)d,%(INPUT_Y)d -c:v libx264 -preset fast -pix_fmt yuv420p -s %(OUTPUT_WIDTH)dx%(OUTPUT_HEIGHT)d -threads 0 -f flv \"rtmp://live.twitch.tv/app/%(STREAM_KEY)s\""
-    custom_audio = " -i %(AUDIO_FILE)s"
+    command = "ffmpeg -f x11grab -s %(INPUT_WIDTH)dx%(INPUT_HEIGHT)d -r %(FPS)d -i %(DISPLAY)s+%(INPUT_X)d,%(INPUT_Y)d -c:v libx264 -preset fast -pix_fmt yuv420p -s %(OUTPUT_WIDTH)dx%(OUTPUT_HEIGHT)d -c:a libmp3lame -ab 96k -ar 22050 -threads 0 -f flv \"rtmp://live.twitch.tv/app/%(STREAM_KEY)s\""
+    audio_pulse = " -f pulse -ac 2 -i default"
+    audio_custom = " -i %(AUDIO_FILE)s"
     windowed_output = " -vcodec rawvideo -pix_fmt yuv420p -window_size 1280x720 -f sdl \"scrapstream output\""
 
     def __init__(self):
@@ -28,7 +29,9 @@ class FFMpegManager:
     def start(self):
         ffmpeg_command = self.get_command()
         if(StreamSettings.custom_audio):
-            ffmpeg_command += FFMpegManager.custom_audio % {'AUDIO_FILE': StreamSettings.audio_file }
+            ffmpeg_command += FFMpegManager.audio_custom % {'AUDIO_FILE': StreamSettings.audio_file }
+        else:
+            ffmpeg_command += FFMpegManager.audio_pulse # TODO add support for ALSA, input source selection
 
         ffmpeg_split_command = shlex.split(ffmpeg_command)
         print("Running FFMpeg: " , ffmpeg_split_command)
